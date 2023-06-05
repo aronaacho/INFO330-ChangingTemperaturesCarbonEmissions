3aede015c205600223d25246c407fd5b7c61f5fd
katie1616 <khulst@uw.edu>
2023-06-04 18:02:25 -0700

    csv for boxplot

--- queries.sql
+++
@@ -1,6 +0,0 @@
--- Finding the total change in carbon emissions over all the years  (Range)
-SELECT (MAX(Y2019) - MIN(Y2000)) FROM countries_emissions;
-
--- Finding the total change in temperature over all the years (Range)
-SELECT (MAX(Y2019) - MIN(Y2000)) FROM countries_temp;
-
